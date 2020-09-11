999b37ec3261394fdfa582b6fcc317b67fc2d4e1
Likoo <43299924+ookil@users.noreply.github.com>
2020-09-11 10:31:44 +0200

    changed page icon and readme

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,11 +6,13 @@ const About = () => {
       <h3>Hi,</h3>
       <p>I created this app as part of my learning journey.</p>
       <p>
-        It uses <a aria-label="Jikan API link" href="https://jikan.docs.apiary.io/" style={{color: "skyblue"}} >Jikan API </a>
+        It uses <a aria-label="Jikan API" href="https://jikan.docs.apiary.io/" title="Jikan API" style={{color: "skyblue"}} >Jikan API </a>
         for searching animes.
       </p>
       <br/>
       <p><strong>Developer: </strong><a href="https://github.com/ookil?tab=repositories">ookil</a></p>
+      <br/>
+      <small>Icons made by <a href="https://www.flaticon.com/authors/smalllikeart" title="smalllikeart" style={{color: "skyblue"}}>smalllikeart</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></small>
     </div>
   );
 };
